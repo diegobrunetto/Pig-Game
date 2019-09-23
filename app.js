97a92ed855830eaf014497e2efed7abc0f008268
diegobrunetto <diego.brunetto@hotmail.com>
2019-09-23 20:08:14 -0300

    change display in the dice

--- app.js
+++ app.js
@@ -38,8 +38,6 @@ function changePlayerTurn() {
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
 
-    diceDOM.style.display = 'none';
-
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
 }
